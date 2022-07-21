058d683bc6b18a22acac134d542ea9d354b9b05a
nemo <fageaf@gmail.com>
2022-07-21 13:53:39 +0300

    Мелкие правки.

--- NetSeaFight/src/lobby/network/Client.java
+++ NetSeaFight/src/lobby/network/Client.java
@@ -1,49 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lobby.network;
 
 import java.io.*;
 import java.net.*;
 
 public class Client {
-    
-  public static void main(String[] args) throws IOException {
-    System.out.println("Клиент стартовал");
-    Socket server = null;
-    String address = "localhost";
-    //  адрес (имя) сервера должны передаваться как параметр
-    //if (args.length==0) {
-    //  System.out.println("Использование: java Client hostname");
-    //  System.exit(-1);
-    //}
-    
-System.out.println("Соединяемся с сервером "+address);
 
-server = new Socket(address,6666);
-BufferedReader in  = new BufferedReader(
-   new  InputStreamReader(server.getInputStream()));
-PrintWriter out =
-   new PrintWriter(server.getOutputStream(),true);
-BufferedReader inu =
-   new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        System.out.println("Клиент стартовал");
+        Socket server = null;
+        String address = "localhost";
+        //  адрес (имя) сервера должны передаваться как параметр
+        //if (args.length==0) {
+        //  System.out.println("Использование: java Client hostname");
+        //  System.exit(-1);
+        //}
 
-String fuser,fserver;
+        System.out.println("Соединяемся с сервером " + address);
 
-while ((fuser = inu.readLine())!=null) {
-   out.println(fuser);
-   fserver = in.readLine();
-   System.out.println(fserver);
-   if (fuser.equalsIgnoreCase("close"))
-     break;
-   if (fuser.equalsIgnoreCase("exit"))
-     break;
- }
-    out.close();
-    in.close();
-    inu.close();
-    server.close();
-  }
+        server = new Socket(address, 6666);
+        BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+        PrintWriter out = new PrintWriter(server.getOutputStream(), true);
+        BufferedReader inu = new BufferedReader(new InputStreamReader(System.in));
+
+        String fuser, fserver;
+
+        while ((fuser = inu.readLine()) != null) {
+            out.println(fuser);
+            fserver = in.readLine();
+            System.out.println(fserver);
+            if (fuser.equalsIgnoreCase("close")) {
+                break;
+            }
+            if (fuser.equalsIgnoreCase("exit")) {
+                break;
+            }
+        }
+        out.close();
+        in.close();
+        inu.close();
+        server.close();
+    }
 }
