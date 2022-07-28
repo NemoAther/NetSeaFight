e99562338c5329d0cdc6cc68940fc5c100abc5ef
nemo <fageaf@gmail.com>
2022-07-28 15:40:45 +0300

    Добавлен разбор сообщения. Пока не работает. Но надо отправить.

--- NetSeaFight/src/lobby/network/GameServer.java
+++ NetSeaFight/src/lobby/network/GameServer.java
@@ -1,30 +1,24 @@
 package lobby.network;
 
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-
 /**
  *
  * @author ksmnote
  */
 public class GameServer implements Runnable {
 
-    private ServerOne playerOne;
-    private ServerOne playerTwo;
-    private boolean gameStarted = false;
+    private PlayerMirror playerOne;
+    private PlayerMirror playerTwo;
+    private boolean gameOn = false;
     private int playersCount = 0;
-    private BufferedReader inOne;
-    private BufferedReader inTwo;
-    private PrintWriter out;
     volatile int turn = 0;
 
-    public GameServer(ServerOne player) {
+    public GameServer(PlayerMirror player) {
         playerOne = player;
         playersCount++;
         System.out.println("Игрок 1 присоединился");
     }
 
-    public String addPlayer(ServerOne player) {
+    public String addPlayer(PlayerMirror player) {
         if (playerTwo == null && playersCount == 1) {
             playerTwo = player;
             playersCount++;
@@ -42,36 +36,91 @@ public class GameServer implements Runnable {
             playerOne.playerInGameID = 1;
             playerTwo.playerInGameID = 2;
             System.out.println("Игра началась");
-            gameStarted = true;
+            gameOn = true;
             turn = playerOne.playerInGameID;
-            while (gameStarted) {
-                
-                //System.out.println("playerID= " + playerOne.playerInGameID + " turn =" + turn);
+            while (gameOn) {
                 if (playerOne.playerInGameID == turn && playerOne.hasMessage) {
-                    System.out.println("Игрок " + playerOne.playerInGameID + " прислал сообщение: " + playerOne.message);
-                    playerOne.hasMessage = false;
-                    //System.out.println("playerOne.hasMessage " + playerOne.hasMessage);
-                    playerOne.message = null;
-                    //System.out.println("playerOne.message " + playerOne.message);
-                    turn = playerTwo.playerInGameID;
-                    System.out.println("turnOnServer " + turn);
-                } 
-                if (playerTwo.playerInGameID == turn && playerTwo.hasMessage) {
-                    //System.out.println("зачем я здесь? ");
-                    System.out.println("Игрок " + playerTwo.playerInGameID + " прислал сообщение: " + playerTwo.message);
-                    playerTwo.hasMessage = false;
-                    playerTwo.message = null;
-                    turn = playerOne.playerInGameID;
+                    processingMessage(playerOne.message, 1, 2);
+                    setTurn(playerTwo);
                 }
+                if (playerTwo.playerInGameID == turn && playerTwo.hasMessage) {
+                    processingMessage(playerTwo.message, 2, 1);
+                    setTurn(playerOne);
+                }
+                /*
+                server.message(string, 0, playerID)
+                
+                
+                 */
             }
         }
     }
 
     public boolean isStarted() {
-        return gameStarted;
+        return gameOn;
     }
 
     public int getPlayersCount() {
         return playersCount;
+    }
+
+    /*public void messagePlayerOne() {
+        //System.out.print("Игрок " + playerOne.playerInGameID + " прислал сообщение: " + playerOne.message);
+        synchronized (this) {
+            playerTwo.setMessage(playerOne.message);
+        }
+        playerOne.hasMessage = false;
+        playerOne.message = null;
+        turn = playerTwo.playerInGameID;
+    }*/
+    public void processingMessage(PlayerMirror sender) {
+        //System.out.print("Игрок " + playerTwo.playerInGameID + " прислал сообщение: " + playerTwo.message);
+        synchronized (this) {
+            if (sender == playerOne) {
+                playerTwo.setMessage(playerOne.message);
+            } else {
+                playerOne.setMessage(playerTwo.message);
+            }
+        }
+        sender.hasMessage = false;
+        sender.message = null;
+        turn = playerOne.playerInGameID;
+    }
+
+    public void setTurn(PlayerMirror receiver) {
+        turn = receiver.playerInGameID;
+    }
+
+    /**
+     * @param rawMessage исходное сообщение для разбора
+     * @param sender отправитель 0 - сервер, 1 - первый игрок, 2 - второй игрок
+     * @param reseiver адресат 0 - сервер, 1 - первый игрок, 2 - второй игрок
+     */
+    public void processingMessage(String rawMessage, int sender, int reseiver) {
+
+        String[] message = rawMessage.split(";"); //добавить ограничитель, чтобы делил только по первому вхождению
+        switch (message[0]) {
+            case "shoot":
+                shoot(message[1], sender, reseiver);
+                break; //выстрел: координаты
+            case "msg":
+                chatMessage(message[1], sender, reseiver);
+                break; //просто сообщение - переслать в чат
+            case "serv":
+                serviceMessage(message[1], sender, reseiver);
+                break; //сервисноее сообщение - начало игры, конец игры, дисконнект, передача игрового поля итд
+        }
+    }
+
+    public void chatMessage(String message, int sender, int reseiver) {
+
+    }
+
+    public void serviceMessage(String message, int sender, int reseiver) { //
+
+    }
+
+    public void shoot(String message, int sender, int reseiver) { //нужен только отправитель - получатель всегда сервер, но унифицируем логику
+
     }
 }
