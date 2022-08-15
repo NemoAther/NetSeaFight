66fbd845f1db6c0ab779f6ab3f0a8c3ea7ee3ffc
nemo <fageaf@gmail.com>
2022-08-15 16:21:39 +0300

    Рисуется сетка и выделяется ячейка

--- NetSeaFight/src/client/GUI/FightField.java
+++ NetSeaFight/src/client/GUI/FightField.java
@@ -1,15 +1,9 @@
 package client.GUI;
 
 import java.awt.Color;
-import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Point;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
-import static java.lang.Thread.sleep;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JPanel;
 
 /**
@@ -18,34 +12,55 @@ import javax.swing.JPanel;
  */
 public class FightField extends JPanel implements Runnable {
 
+    int gridSize = 10;
+    int cellSize = 30;
     public static final int CANVAS_WIDTH = 800;
     public static final int CANVAS_HEIGHT = 600;
     Point cursor = new Point(0, 0);
     volatile int x1 = 0;
     volatile int y1 = 0;
-    volatile int x2 = 10;
-    volatile int y2 = 10;
+    volatile int x2 = 5;
+    volatile int y2 = 5;
+   
+    int xSelect = 0;
+    int ySelect = 0;
 
     public FightField() {
-        setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));        
-        
+        setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
+
         Thread redraw = new Thread(this);
         redraw.start();
     }
-    
+
     @Override
     public void run() {
-        int direction = 1;
         while (true) {
             repaint();
+            if(x1 >= cellSize && x1 < cellSize*(gridSize+1)) {
+                if(y1 >= cellSize && y1 < cellSize*(gridSize+1)) {
+                    xSelect = ((int)x1/cellSize) * cellSize;
+                    ySelect = ((int)y1/cellSize) * cellSize;
+                }
+            }
         }
     }
 
-    void draw(Graphics g
-    ) {
+    void draw(Graphics g) {
         g.setColor(Color.red);
         //g.drawLine(x1, y1, x2, y2);
         g.drawRect(x1, y1, x2, y2);
+        drawGrid(g);
+        g.setColor(Color.red);
+        g.drawRect(xSelect, ySelect, cellSize, cellSize);
+    }
+
+    private void drawGrid(Graphics g) {
+        g.setColor(Color.yellow);
+        //g.drawLine(10, 10, 310, 10);
+        for (int i = 1; i < gridSize + 2; i++) {
+            g.drawLine(cellSize, i*cellSize, cellSize*(gridSize+1), i*cellSize);
+            g.drawLine(i*cellSize, cellSize, i*cellSize, cellSize*(gridSize+1));
+        }
     }
 
     @Override
@@ -55,5 +70,4 @@ public class FightField extends JPanel implements Runnable {
         setBackground(Color.BLACK);
         draw(g);
     }
-
 }
--- NetSeaFight/src/client/GUI/MainWindow.java
+++ NetSeaFight/src/client/GUI/MainWindow.java
@@ -6,12 +6,9 @@
 package client.GUI;
 
 import java.awt.FlowLayout;
-import java.awt.MouseInfo;
-import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
 
 /**
  *
@@ -27,24 +24,21 @@ public class MainWindow extends JFrame {
         setLayout(new FlowLayout());
         FightField fightField = new FightField();
         add(fightField);
-        
+
         fightField.addMouseMotionListener(new MouseMotionListener() {
             @Override
             public void mouseMoved(MouseEvent e) {
                 fightField.cursor = e.getLocationOnScreen();
                 fightField.x1 = e.getX();
-                fightField.y1 = e.getY();
+                fightField.y1 = e.getY();                
             }
 
             @Override
             public void mouseDragged(MouseEvent e) {
-           
+
             }
         });
-        
-        
-        
-        
+
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
         setVisible(true);
